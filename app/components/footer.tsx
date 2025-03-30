b801c7c0d3486f9256956bf5de02ecf9be16d877
DANCAN KARANI <karanidancan120@gmail.com>
2025-03-30 21:40:53 +0300

    my commit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <footer className="bg-gray-800 text-white py-8 text-center">
-        <p>📍 Kenyatta University | 📞 +254 712 345 678 | ✉️ support@campusmarket.com</p>
+        <p>📍 Kenyatta University | 📞 +254 707 895 563 | ✉️ support@campusmarket.com</p>
         <p className="mt-4">&copy; {new Date().getFullYear()} Campus Market. All Rights Reserved.</p>
       </footer>
     )
